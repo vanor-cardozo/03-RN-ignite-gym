20d8b23b31df8acdbb280e673fdd8d7971fa2ad0
vanor-cardozo <vanor.fc@gmail.com>
2023-06-02 17:03:49 -0300

    feat: Personalizando a StatusBar

--- App.tsx
+++ App.tsx
@@ -1,4 +1,4 @@
-import { Text, View } from "react-native";
+import { StatusBar, Text, View } from "react-native";
 import {
   useFonts,
   Roboto_400Regular,
@@ -8,9 +8,27 @@ import {
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
   return (
-    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+    <View
+      style={{
+        flex: 1,
+        alignItems: "center",
+        justifyContent: "center",
+        backgroundColor: "#202024",
+      }}
+    >
+      <StatusBar
+        barStyle="light-content"
+        backgroundColor="transparent"
+        translucent
+      />
       {fontsLoaded ? (
-        <Text style={{ fontSize: 32, fontFamily: "Roboto_700Bold" }}>
+        <Text
+          style={{
+            fontSize: 32,
+            color: "#fafafa",
+            fontFamily: "Roboto_700Bold",
+          }}
+        >
           Ignite Gym
         </Text>
       ) : (
