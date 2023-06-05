87eb54a4ee8283ab05c7c24051c3f1fce6718301
vanor-cardozo <vanor.fc@gmail.com>
2023-06-05 18:02:58 -0300

    feat: Rotas privadas
    Implementa o Bottom Navigator, estilização com ícones SVG, tipagem para rotas

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -3,6 +3,7 @@ import { NavigationContainer, DefaultTheme } from "@react-navigation/native";
 import { Box, useTheme } from "native-base";
 
 import { AuthRoutes } from "./auth.routes";
+import { AppRoutes } from "./app.routes";
 
 export function Routes() {
   const { colors } = useTheme();
@@ -13,7 +14,8 @@ export function Routes() {
   return (
     <Box flex={1} bg="gray.700">
       <NavigationContainer>
-        <AuthRoutes />
+        {/* <AuthRoutes /> */}
+        <AppRoutes />
       </NavigationContainer>
     </Box>
   );
