8394437f116eef5990852ac7593996f1921785ac
vanor-cardozo <vanor.fc@gmail.com>
2023-06-06 14:54:45 -0300

    refactor: Ajuste para responsividade de telas menores

--- src/components/HistoryCard.tsx
+++ src/components/HistoryCard.tsx
@@ -11,8 +11,13 @@ export function HistoryCard() {
       alignItems="center"
       justifyContent="space-between"
     >
-      <VStack>
-        <Heading color="white" fontSize="md" textTransform="capitalize">
+      <VStack mr={5} flex={1}>
+        <Heading
+          color="white"
+          fontSize="md"
+          textTransform="capitalize"
+          numberOfLines={1}
+        >
           Costas
         </Heading>
 
